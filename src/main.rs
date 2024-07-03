8df86656fe72ede28a39b3007a7a3bf3b0ffea13
Shuflduf <shuflduf@gmail.com>
2024-07-03 13:59:02 -0600

    moved the ground to the bottom

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
         clear_background(RED);
 
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_rectangle(0.0, screen_height() - 100.0, screen_width(), 100.0, GREEN);
         draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
 
         draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
